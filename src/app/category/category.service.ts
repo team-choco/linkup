88bab53c930d01ca7e15ac8d27f8351cdc59d932
tnrms08 <wjddlfla661@naver.com>
2023-01-30 14:04:46 +0900

    link-collection-api 연결

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {LinkCollection} from "../link-collection/link-collection.types";
 import {Category} from "./category.types";
+import {ActivatedRoute} from "@angular/router";
+import {LinkCollectionService} from "../link-collection/link-collection.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,15 @@ import {Category} from "./category.types";
 export class CategoryService {
 
   constructor(
-    private httpClient:HttpClient
-  ) { }
+    private httpClient:HttpClient,
+
+  ) {
+
+  }
 
   getCategories():Observable<Category[]>{
     return this.httpClient.get<Category[]>("http://localhost:8080/categories")
   }
-  getLinkCollections():Observable<LinkCollection[]>{
-    return this.httpClient.get<LinkCollection[]>("http://localhost:8080/link-collections")
-  }
+
+
 }
