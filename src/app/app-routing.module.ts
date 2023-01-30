7bf88319d24872df69eb9034e1915532dfc2b240
1jinju <gkswlswn2044@gmail.com>
2023-01-30 15:28:20 +0900

    layout 분리

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LinkCollectionComponent} from "./link-collection/link-collection.component";
 import {LinkComponent} from "./link/link.component";
 import {LinkCollectionDetailComponent} from "./link-collection/link-collection-detail/link-collection-detail.component";
+import {DefaultLayoutComponent} from "./layout/default/default-layout.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   {
-    path:"link-collection",
-    component:LinkCollectionComponent,
-    children:[
-      {path:":id", component:LinkCollectionDetailComponent}
+    path: '', pathMatch: 'full', redirectTo: '/link'
+  },
+  {
+    path: "link",
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: "",
+        component: LinkCollectionComponent
+      },
+      {
+        path: ":id",
+        component: LinkCollectionDetailComponent
+      },
     ]
   },
-  { path: 'links', component: LinkComponent},
-  ];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
